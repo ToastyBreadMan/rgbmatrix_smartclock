adf12c50aa06ea934d1703831417ee62ca23d1b4
MacLean <smaclean@vols.utk.edu>
2021-04-21 22:33:45 -0400

    Added place for keys

---
+++ esp32_clock/include/key.h
@@ -0,0 +1,4 @@
+// Initialize Wifi connection to the router
+char ssid[] = "";		// your network SSID (name)
+char password[] = "";		// your network 
+char openweatherapi[] = ""; 	// openweather api key
